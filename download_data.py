f20ccb63485cc0256fd6f4da4f7bc52aecb43813
tiginamaria <tiginamaria@gmail.com>
2022-06-14 11:51:18 +0200

    Add "дорешка" calculator

---
+++ download_data.py
@@ -0,0 +1,73 @@
+import argparse
+import os
+import sys
+
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_table_from_page(page):
+    html = BeautifulSoup(page.text)
+    table = html.find("table", {"class": "mtab"})
+
+    header = []
+    for cell in table.findAll("tr", {"class": "head"})[0].findAll("th")[1:-3]:
+        header.append(cell.text)
+
+    results = [header]
+    for row in table.findAll("tr", {"class": ['even', 'odd']})[:-4]:
+        result = []
+        cells = row.findAll("td")
+        result.append(cells[1].text[4:])
+        for cell in cells[2:-3]:
+            result.append(cell['class'][0])
+        results.append(result)
+
+    results[0][0] = 'User'
+
+    return results
+
+
+def download_monitor(login: str, token: str, contests_path: str, monitors_path: str):
+    df_contests = pd.read_csv(contests_path)
+
+    if not os.path.exists(monitors_path):
+        os.makedirs(monitors_path)
+
+    for contest in df_contests['contest'].values:
+        page = requests.get("https://acm.math.spbu.ru/tsweb/monitor",
+                            cookies={'freshness': '5', 'tsw': f'{login}|{token}|{contest}|'},
+                            headers={'Cache-Control': 'no-cache'})
+        results = get_table_from_page(page)
+        pd.DataFrame(results[1:], columns=results[0]) \
+            .to_csv(os.path.join(monitors_path, f'{contest}.csv'), index=False)
+
+
+def download_results(contests_path: str, results_path: str):
+    df_contests = pd.read_csv(contests_path)
+
+    if not os.path.exists(results_path):
+        os.makedirs(results_path)
+
+    for contest in df_contests['contest'].values:
+        page = requests.get(f"http://acm.math.spbu.ru/cgi-bin/monitor_au.pl/m{contest}.dat")
+        results = get_table_from_page(page)
+        pd.DataFrame(results[1:], columns=results[0]) \
+            .to_csv(os.path.join(results_path, f'{contest}.csv'), index=False)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('contests_path', type=str, help='Path to .csv file with list of contests.')
+    parser.add_argument('monitors_path', type=str, help='Path to directory where to save monitors.')
+    parser.add_argument('results_path', type=str, help='Path to directory where to save results.')
+
+    parser.add_argument('login', type=str, help='Test sys login.')
+    parser.add_argument('token', type=str, help='Test sys token.')
+
+    args = parser.parse_args(sys.argv[1:])
+
+    download_monitor(args.login, args.token, args.contests_path, args.monitors_path)
+    download_results(args.contests_path, args.results_path)
---
+++ search_data.py
@@ -0,0 +1,68 @@
+import argparse
+import os
+import sys
+
+import pandas as pd
+
+
+def get_contest_scores(df: pd.DataFrame, user: str):
+    df_user = df[df['User'] == user]
+    contest_scores = []
+    for i in range(0, ord(df.columns[1][-1]) - ord('A')):
+        contest_scores.append('')
+    if df_user.shape[0] > 0:
+        contest_scores += list(df_user.iloc[0].values)[1:]
+    else:
+        contest_scores += list(['no'] * (df.shape[1] - 1))
+
+    return contest_scores
+
+
+def compare_contest_scores(user_results, user_monitor):
+    contest_scores = []
+    for (result, monitor) in zip(user_results, user_monitor):
+        if result not in ['ok', 'firstokeven'] and monitor in ['ok', 'firstokeven']:
+            contest_scores.append(f'[{monitor}]')
+        else:
+            contest_scores.append(monitor)
+    return contest_scores
+
+
+def search_user_results(contests_path: str, monitors_path: str, results_path: str, scores_path: str, users_path: str):
+    df_users = pd.read_csv(users_path)
+    df_contests = pd.read_csv(contests_path)
+
+    for user in df_users['User'].values:
+        contests_scores = []
+
+        for contest in df_contests['contest']:
+            df_results = pd.read_csv(os.path.join(results_path, f'{contest}.csv'))
+            df_monitor = pd.read_csv(os.path.join(monitors_path, f'{contest}.csv'))
+            user_results = get_contest_scores(df_results, user)
+            user_monitor = get_contest_scores(df_monitor, user)
+            contest_scores = compare_contest_scores(user_results, user_monitor)
+
+            contests_scores.append([contest, len(contest_scores)] + contest_scores)
+
+        size = max(list(map(lambda result: len(result), contests_scores)))
+        for contest_scores in contests_scores:
+            if len(contest_scores) < size:
+                contest_scores += [''] * (size - len(contest_scores))
+
+        columns = [chr(ord('A') + i) for i in range(0, size - 2)]
+        pd.DataFrame(contests_scores, columns=['Contest', 'Total'] + columns) \
+            .to_csv(os.path.join(scores_path, f'{user}.csv'), index=False, sep='\t')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('contests_path', type=str, help='Path to .csv file with list of contests.')
+    parser.add_argument('monitors_path', type=str, help='Path to directory where to save monitors.')
+    parser.add_argument('results_path', type=str, help='Path to directory where to save results.')
+    parser.add_argument('scores_path', type=str, help='Path to directory where to save results.')
+    parser.add_argument('users_path', type=str, help='Path to directory where to save results.')
+
+    args = parser.parse_args(sys.argv[1:])
+
+    search_user_results(args.contests_path, args.monitors_path, args.results_path, args.scores_path, args.users_path)
